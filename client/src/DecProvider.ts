d210d0d5d1b7604eb80a543d475762e4cddfa153
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-02-14 13:30:37 +0100

    Fix imports in declaration provider

--- client/src/DecProvider.ts
+++ client/src/DecProvider.ts
@@ -2,8 +2,8 @@ import { pathToFileURL } from "url";
 import { CancellationToken, Declaration, DeclarationProvider, Location, Position, ProviderResult, TextDocument, Uri } from "vscode";
 import { RequestType, VersionedTextDocumentIdentifier } from "vscode-languageclient";
 import Client from "./client";
-import { CompletionItemCoq, CompletionItemCoqRequest, CompletionItemCoqResponse, DeclarationLocationCoqRequest, DeclarationLocationCoqResponse } from "./protocol/types";
-import * as fs from 'fs';
+import { DeclarationLocationCoqRequest, DeclarationLocationCoqResponse } from "./protocol/types";
+import { existsSync } from 'fs';
 
 export default class DecProvider implements DeclarationProvider {
     client: Client;
@@ -18,7 +18,7 @@ export default class DecProvider implements DeclarationProvider {
         let requestedDeclaration = document.getText(wordRange);
         let response = await this.sendDeclarationLocationRequest(document.uri, document.version, position, requestedDeclaration);
         let path = response.path.replace(/\.vo$/, ".v");
-        if (path && fs.existsSync(path)) {
+        if (path && existsSync(path)) {
             return new Location(Uri.parse(path), new Position(0, 0));
         }
         throw new Error(`Could not find path to ${requestedDeclaration}`);
